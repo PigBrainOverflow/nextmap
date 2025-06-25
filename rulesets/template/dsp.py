d1682a6cd1ee283f7231ef591cb6562186271d24
Sijie Kong <sijie_kong@ucsb.edu>
2025-06-25 21:11:49 +0000

    Added 2 rules in dsp.py

--- rulesets/template/dsp.py
+++ rulesets/template/dsp.py
@@ -26,14 +26,33 @@ if __name__ == "__main__":
     ))
 
 
+    ########################################
+    # 1-stage 26-17-48-bit unsigned mulsub #
+    ########################################
+    a = pg.BasicDialect.InputOp("a", 26)
+    b = pg.BasicDialect.InputOp("b", 17)
+    c = pg.BasicDialect.InputOp("c", 48)
+    clk = pg.BasicDialect.InputOp("clk", 1)
+
+    a_mul_b = pg.BasicDialect.ExtractOp(pg.ArithDialect.MulOp(a, b), 42, 0)
+    a_mul_b_sub_c = pg.BasicDialect.ExtractOp(pg.ArithDialect.SubOp(a_mul_b, c), 47, 0)
+    a_mul_b_sub_c_delay_1 = pg.BasicDialect.DelayOp(clk, a_mul_b_sub_c)
+    out = pg.BasicDialect.OutputOp("out", pg.BasicDialect.ExtractOp(a_mul_b_sub_c_delay_1, 47, 0))
+
+    prims.append((
+        "unsigned_mulsub_1_stage_26_17_48_bit",
+        [out],
+        [a, b, c],
+        1,
+        "clk"  # clock for 1-stage
+    ))
+
+
     ##############################################
     # 1-stage 25-17-48-25-bit unsigned addmuladd #
     ##############################################
     a = pg.BasicDialect.InputOp("a", 25)
-    b = pg.BasicDialect.InputOp("b", 17)
-    c = pg.BasicDialect.InputOp("c", 48)
     d = pg.BasicDialect.InputOp("d", 25)
-    clk = pg.BasicDialect.InputOp("clk", 1)
 
     a_add_d = pg.BasicDialect.ExtractOp(pg.ArithDialect.AddOp(a, d), 25, 0)
     a_add_d_mul_b = pg.BasicDialect.ExtractOp(pg.ArithDialect.MulOp(a_add_d, b), 42, 0)
@@ -43,6 +62,62 @@ if __name__ == "__main__":
 
     prims.append((
         "unsigned_addmuladd_1_stage_25_17_48_25_bit",
+        [out],
+        [a, b, c, d],
+        1,
+        "clk"  # clock for 1-stage
+    ))
+
+
+    ##############################################
+    # 1-stage 25-17-48-25-bit unsigned addmulsub #
+    ##############################################
+    a_add_d = pg.BasicDialect.ExtractOp(pg.ArithDialect.AddOp(a, d), 25, 0)
+    a_add_d_mul_b = pg.BasicDialect.ExtractOp(pg.ArithDialect.MulOp(a_add_d, b), 42, 0)
+    a_add_d_mul_b_sub_c = pg.BasicDialect.ExtractOp(pg.ArithDialect.SubOp(a_add_d_mul_b, c), 47, 0)
+    a_add_d_mul_b_sub_c_delay_1 = pg.BasicDialect.DelayOp(clk, a_add_d_mul_b_sub_c)
+    out = pg.BasicDialect.OutputOp("out", pg.BasicDialect.ExtractOp(a_add_d_mul_b_sub_c_delay_1, 47, 0))
+
+    prims.append((
+        "unsigned_addmulsub_1_stage_25_17_48_25_bit",
+        [out],
+        [a, b, c, d],
+        1,
+        "clk"  # clock for 1-stage
+    ))
+
+
+    # failure
+    ##############################################
+    # 1-stage 25-17-48-25-bit unsigned submuladd #
+    ##############################################
+    a_sub_d = pg.BasicDialect.ExtractOp(pg.ArithDialect.SubOp(a, d), 25, 0)
+    a_sub_d_mul_b = pg.BasicDialect.ExtractOp(pg.ArithDialect.MulOp(a_sub_d, b), 42, 0)
+    a_sub_d_mul_b_add_c = pg.BasicDialect.ExtractOp(pg.ArithDialect.AddOp(a_sub_d_mul_b, c), 47, 0)
+    a_sub_d_mul_b_add_c_delay_1 = pg.BasicDialect.DelayOp(clk, a_sub_d_mul_b_add_c)
+    out = pg.BasicDialect.OutputOp("out", pg.BasicDialect.ExtractOp(a_sub_d_mul_b_add_c_delay_1, 47, 0))
+
+    prims.append((
+        "unsigned_submuladd_1_stage_25_17_48_25_bit",
+        [out],
+        [a, b, c, d],
+        1,
+        "clk"  # clock for 1-stage
+    ))
+
+
+    # failure
+    ##############################################
+    # 1-stage 25-17-48-25-bit unsigned submulsub #
+    ##############################################
+    a_sub_d = pg.BasicDialect.ExtractOp(pg.ArithDialect.SubOp(a, d), 25, 0)
+    a_sub_d_mul_b = pg.BasicDialect.ExtractOp(pg.ArithDialect.MulOp(a_sub_d, b), 42, 0)
+    a_sub_d_mul_b_sub_c = pg.BasicDialect.ExtractOp(pg.ArithDialect.SubOp(a_sub_d_mul_b, c), 47, 0)
+    a_sub_d_mul_b_sub_c_delay_1 = pg.BasicDialect.DelayOp(clk, a_sub_d_mul_b_sub_c)
+    out = pg.BasicDialect.OutputOp("out", pg.BasicDialect.ExtractOp(a_sub_d_mul_b_sub_c_delay_1, 47, 0))
+
+    prims.append((
+        "unsigned_submulsub_1_stage_25_17_48_25_bit",
         [out],
         [a, b, c, d],
         1,
