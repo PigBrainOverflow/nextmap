019616e63928e11b1d8556e9c5727384e46e5356
Sijie Kong <sijie_kong@ucsb.edu>
2025-07-02 00:10:49 +0000

    Fixed missing '\' module_name

--- blackbox.v
+++ blackbox.v
@@ -1,26 +1,8 @@
-module adder_8_bit(
-    input [7:0] a,
-    input [7:0] b,
-    input cin,
-    output [7:0] sum,
-    output cout
+module unsigned_extension_test (
+    input [3:0] a,
+    input [3:0] b,
+    output [7:0] out
 );
-    wire cout0;
-
-    adder_4_bit adder0 (
-        .a(a[3:0]),
-        .b(b[3:0]),
-        .cin(cin),
-        .sum(sum[3:0]),
-        .cout(cout0)
-    );
-
-    adder_4_bit adder1 (
-        .a(a[7:4]),
-        .b(b[7:4]),
-        .cin(cout0),
-        .sum(sum[7:4]),
-        .cout(cout)
-    );
+    assign out = a + b;
 
 endmodule
--- emap/src/emap.cpp
+++ emap/src/emap.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 #include <kernel/yosys.h>
-#include <kernel/yosys_common.h>
-#include <kernel/functional.h>
 
-#include "emap/utils.h"
+#include "emap/serialization.h"
 
 
 namespace Yosys {
 
-struct WriteFunctionalPass : public Pass {
+struct WriteStrataPass : public Pass {
 public:
-    WriteFunctionalPass() : Pass("write_functional", "convert module to Functional IR and write to file") {}
+    WriteStrataPass() : Pass("write_strata", "write Strata IR to a file") {}
 
     void execute(std::vector<std::string> args, RTLIL::Design *design) override {
         if (args.size() < 2 || args.size() > 3) {
-            log_error("Usage: write_functional <module_name> [<filename>]\n");
+            log_error("Usage: write_strata <module_name> [<filename>]\n");
         }
 
         bool to_file = args.size() == 3;
@@ -23,62 +21,18 @@ public:
             log_error("Could not open output stream.\n");
         }
         else {
-            auto module = design->module(args[1]);
+            auto module = design->module("\\" + args[1]);
             if (!module) {
                 log_error("Module '%s' not found in design.\n", args[1].c_str());
             }
             else {
-                
+                *output_stream << Yosys::Json(emap::serialization::serialize(module)).dump();
             }
         }
         if (to_file) {
             delete output_stream;
         }
     }
-};
+} write_strata_pass;
 
 }
-
-
-USING_YOSYS_NAMESPACE
-PRIVATE_NAMESPACE_BEGIN
-
-class PrintVisitor : public Functional::DefaultVisitor<void> {
-public:
-    PrintVisitor(std::ostream &os) : os(os) {}
-    virtual void default_handler(Functional::Node self) override {
-        os << "Node: " << self.to_string() << std::endl;
-    }
-    virtual void buf(Functional::Node self, Functional::Node) override {
-        os << "Buf: " << self.to_string() << self.name().c_str() << self.width() << std::endl;
-    }
-
-private:
-    std::ostream &os;
-};
-
-struct MyPass : public Pass {
-    MyPass() : Pass("my_cmd", "just a simple test") { }
-    void execute(std::vector<std::string> args, RTLIL::Design *design) override
-    {
-        UNUSED(args);
-        Functional::Writer writer(std::cout);
-        PrintVisitor visitor(std::cout);
-        for (auto mod : design->modules()) {
-            std::cout << "Module: " << mod->name.c_str() << std::endl;
-            auto fmod = Functional::IR::from_module(mod);
-            for (const auto& node : fmod) {
-                node.visit(visitor);
-            }
-            for (const auto& input : fmod.all_inputs()) {
-                writer.print("Input: %s (%s)\n", input->name.c_str(), input->kind.c_str());
-            }
-            for (const auto& output : fmod.all_outputs()) {
-                writer.print("Output: %s (%s)\n", output->name.c_str(), output->kind.c_str());
-            }
-        }
-    }
-} MyPass;
-
-
-PRIVATE_NAMESPACE_END
