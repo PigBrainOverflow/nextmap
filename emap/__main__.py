b752bd0ee92dfa4eeffb9e63f5a9c84795686989
Sijie Kong <sijie_kong@ucsb.edu>
2025-07-11 06:40:33 +0000

    Implementing group_wires()

--- emap/__main__.py
+++ emap/__main__.py
@@ -5,46 +5,52 @@ import argparse
 import json
 
 
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("--schema", nargs="?", type=str, default="emap/schema.sql", help="Path to the schema file")
+#     parser.add_argument("--db", nargs="?", type=str, default=":memory:", help="Path to the database file")
+#     parser.add_argument("--design", type=str, help="Path to the design JSON file")
+#     parser.add_argument("--top", type=str, help="Name of the top module")
+#     parser.add_argument("--rules", type=str, help="Path to the directory of ruleset files")
+#     args = parser.parse_args()
+#     db = NetlistDB(schema_file=args.schema, db_file=args.db, cnt=1000)
+
+#     with open(args.design, "r") as f:
+#         mod = json.load(f)
+
+#     with open(f"{args.rules}/dsp.json", "r") as f:
+#         dsp_rules = json.load(f)
+
+#     create_dsp_tables(db, dsp_rules)
+#     db.build_from_json(mod["modules"][args.top])
+
+#     # rewrite_dff_forward_aby_cell(db, ["$adds", "$addu", "$muls", "$mulu"])
+#     # rewrite_dff_forward_aby_cell(db, ["$adds", "$addu", "$muls", "$mulu"])
+#     # rewrite_comm(db, ["$adds", "$addu", "$muls", "$mulu"])
+#     # rewrite_assoc_to_right(db, ["$adds", "$addu", "$muls", "$mulu"])
+#     rewrite_complex_mul(db)
+#     rewrite_dff_backward_aby_cell(db, ["$adds", "$addu", "$subs", "$subu", "$muls", "$mulu"])
+
+#     print(rewrite_dsp(db, dsp_rules[-2]))
+
+#     greedy.fix_dsps(db, "dsp48e2", 3)
+#     new_design = greedy.extract_dsps_bottom_up(
+#         db,
+#         "dsp48e2",
+#         cost_model=lambda x: 100 if x[0] == "$muls" else 1
+#     )
+
+#     with open("out.json", "w") as f:
+#         json.dump(db.dump_tables(), f, indent=2)
+
+#     with open("out_design.json", "w") as f:
+#         json.dump(
+#             {"creator": "nextmap", "modules": {args.top: new_design}},
+#             f, indent=2
+#         )
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--schema", nargs="?", type=str, default="emap/schema.sql", help="Path to the schema file")
-    parser.add_argument("--db", nargs="?", type=str, default=":memory:", help="Path to the database file")
-    parser.add_argument("--design", type=str, help="Path to the design JSON file")
-    parser.add_argument("--top", type=str, help="Name of the top module")
-    parser.add_argument("--rules", type=str, help="Path to the directory of ruleset files")
-    args = parser.parse_args()
-    db = NetlistDB(schema_file=args.schema, db_file=args.db, cnt=1000)
-
-    with open(args.design, "r") as f:
-        mod = json.load(f)
-
-    with open(f"{args.rules}/dsp.json", "r") as f:
-        dsp_rules = json.load(f)
-
-    create_dsp_tables(db, dsp_rules)
-    db.build_from_json(mod["modules"][args.top])
-
-    # rewrite_dff_forward_aby_cell(db, ["$adds", "$addu", "$muls", "$mulu"])
-    # rewrite_dff_forward_aby_cell(db, ["$adds", "$addu", "$muls", "$mulu"])
-    # rewrite_comm(db, ["$adds", "$addu", "$muls", "$mulu"])
-    # rewrite_assoc_to_right(db, ["$adds", "$addu", "$muls", "$mulu"])
-    rewrite_complex_mul(db)
-    rewrite_dff_backward_aby_cell(db, ["$adds", "$addu", "$subs", "$subu", "$muls", "$mulu"])
-
-    print(rewrite_dsp(db, dsp_rules[-2]))
-
-    greedy.fix_dsps(db, "dsp48e2", 3)
-    new_design = greedy.extract_dsps_bottom_up(
-        db,
-        "dsp48e2",
-        cost_model=lambda x: 100 if x[0] == "$muls" else 1
-    )
-
-    with open("out.json", "w") as f:
-        json.dump(db.dump_tables(), f, indent=2)
-
-    with open("out_design.json", "w") as f:
-        json.dump(
-            {"creator": "nextmap", "modules": {args.top: new_design}},
-            f, indent=2
-        )
+    from emap.extracts.ilp import _group_wires
+    bundles = [{1,2}, {1,2,3}, {3,4}]
+    groups = _group_wires(bundles)
+    print(bundles, groups)
--- emap/extracts/ilp.py
+++ emap/extracts/ilp.py
@@ -1,11 +1,50 @@
 from ..db import NetlistDB
+from .utils import Cell, DFF, db_to_normalized
 import gurobipy
 
 
-def _group_wires(wires: list[set[str]]):
-    
+def _group_wires(bundles: list[set]) -> dict[str, set]:
+    """
+    Return a dictionary mapping each wire group to its corresponding set of wires.
+    Modifies the input list of wires in place.
+    For example, if bundles = [{1,2}, {1,2,3}, {3,4}], the output will be: [{1,2}, {3}, {4}].
+    In most cases, this can significantly reduce the number of wires
+    """
+    groups = {}
+    cnt = 0
+    wires = set()
+    for bundle in bundles:
+        wires.update(bundle)
 
-def extract_dsps(db: NetlistDB, cost_model) -> dict:
+    for wire in wires:
+        wire_group = set()
+        for bundle in bundles:
+            if wire in bundle:
+                wire_group = wire_group.intersection(bundle) if wire_group else {w for w in bundle if not isinstance(w, str) or not w.startswith("group")}
+        if not wire_group:  # this wire is not in any bundle
+            continue
+        groups[f"group{cnt}"] = wire_group
+        # update bundles
+        for bundle in bundles:
+            if wire_group <= bundle:
+                bundle -= wire_group
+                bundle.add(f"group{cnt}")
+        cnt += 1
+
+    return groups
+
+def extract_dsps(db: NetlistDB, name: str, cost_model) -> dict:
+    cells, dffs = db_to_normalized(db, cost_model)
+
+    # it's also possible to let users define the cost model for DSPs
+    # for simplicity, we only consider the DSPs that are already fixed
+    dsp_tables = db.tables_startswith(name)
+    for dsp_table in dsp_tables:
+        cur.execute(f"SELECT rowid, * FROM {dsp_table} WHERE value = 0")
+        cells.update(Cell(table=dsp_table, rowid=row[0], inputs=set(",".join(row[2:-1]).split(",")), outputs=set(row[-1].split(",")), cost=0) for row in cur)
+
+    # call gurobi to solve it
     ilp_model = gurobipy.Model("egraph_extraction")
-
     x = ilp_model.addVars()
+
+    return {}
