40ede55f331f7e22a8fd0c96c5a5ff69a723f268
Sijie Kong <sijie_kong@ucsb.edu>
2025-08-03 22:32:51 +0000

    Fixed bugs in build_from_json()

--- emap/core.py
+++ emap/core.py
@@ -18,6 +18,9 @@ class Wire(egglog.Expr):
 
     def __invert__(self) -> Wire: ...
 
+    @classmethod
+    def mux(cls, a: Wire, b: Wire, s: Wire) -> Wire: ...
+
 class WireVec(egglog.Expr):
     def __init__(self, vec: egglog.Vec[Wire]): ...
 
@@ -56,9 +59,7 @@ class Netlist(egglog.EGraph):
     @staticmethod
     def cell_to_outputs(cell: dict[str, Any]) -> list[str]:
         type_, conns = cell["type"], cell["connections"]
-        if type_ in {"$and", "$or", "$xor"}:
-            return conns["Y"]
-        elif type_ in {"$add", "$mul"}:
+        if type_ in {"$and", "$or", "$xor", "$mux", "$add", "$sub", "$mul"}:
             return conns["Y"]
         elif type_ == "$dff":
             return conns["Q"]
@@ -98,7 +99,7 @@ class Netlist(egglog.EGraph):
     def auto_id(self) -> str:
         # generate a unique ID for each wire or wire vector
         self._cnt += 1
-        return f"tmp{self._cnt}"
+        return f"\\tmp{self._cnt}"
 
     def build_from_json(self, mod: dict, clk: str = "clk"):
         # NOTE: only support single global clock
@@ -126,6 +127,8 @@ class Netlist(egglog.EGraph):
                     ew = self.let(ename, Wire.from_input(ename))
                     wires[w] = ew
                     wire_from[w] = None  # input wires are not connected to any cell
+            elif direction != "output":
+                raise ValueError(f"Unsupported port direction: {direction}")
 
         # TODO: build blackboxes' outputs
 
@@ -140,64 +143,31 @@ class Netlist(egglog.EGraph):
                 raise ValueError(f"Clock {clk} does not match global clock {self._clk}")
             for wq in q:
                 wq = Netlist.bit_to_int(wq)
-                wires[wq] = self.let(str(wq), Wire.from_dff(Wire.from_input(self.auto_id))) # this is a placeholder, will be unioned later
+                wires[wq] = self.let(str(wq), Wire.from_input(self.auto_id)) # this is a placeholder, will be unioned later
+                wire_from[wq] = None
 
         # build cells
         # NOTE: the cells may not be in topological order, so dfs is used to ensure all dependencies are resolved
         for i, cell in enumerate(cells):
             if cell["type"] != "$dff":
                 wire_from.update((Netlist.bit_to_int(bit), i) for bit in Netlist.cell_to_outputs(cell))
-
-    def _build_from_json(self, mod: dict, clk: str = "clk"):
-        # NOTE: only support single global clock
-        # NOTE: not support blackbox cells
-        ports: dict[str, Any] = mod["ports"]
-        cells: list[dict[str, Any]] = [cell for cell in mod["cells"].values()]
-        wires: dict[int, Wire] = {}
-
-        # build inputs
-        for name, port in ports.items():
-            direction, bits = port["direction"], port["bits"]
-            if direction == "input":
-                if name == clk:
-                    if len(bits) != 1:
-                        raise ValueError("Clock port must have exactly one bit")
-                    self._clk = Netlist.bit_to_int(bits[0])
-                wires.update((Netlist.bit_to_int(bit), self.let(f"{name}[{i}]", Wire.from_input(f"{name}[{i}]"))) for i, bit in enumerate(bits))
-
-        # build consts
-        wires[-1] = self.let("x", Wire.from_input("x")) # DC wire, represented as "x" in the netlist
-        wires[0] = self.let("0", Wire.from_input("0"))  # GND wire, represented as "0" in the netlist
-        wires[1] = self.let("1", Wire.from_input("1"))  # VCC wire, represented as "1" in the netlist
-
-        # build cells
-        # NOTE: the cells may not be in topological order, so dfs is used to ensure all dependencies are resolved
-        wire_from: dict[int, int] = {}  # maps wire index to cell index
-        for i, cell in enumerate(cells):
-            wire_from.update((Netlist.bit_to_int(bit), i) for bit in Netlist.cell_to_outputs(cell))
-
         visited = set()
-        def dfs(i: int):
-            if i in visited:
+        def dfs(i: int | None):
+            if i is None or i in visited:
                 return
             cell = cells[i]
             type_, params, conns = cell["type"], cell["parameters"], cell["connections"]
             if type_ in {"$and", "$or", "$xor"}:    # bitwise logic gates, apply bitblast
                 for wa, wb, wy in zip(conns["A"], conns["B"], conns["Y"]):
                     wa, wb, wy = Netlist.bit_to_int(wa), Netlist.bit_to_int(wb), Netlist.bit_to_int(wy)
-                    if wa in wire_from: # not an input wire or const
-                        dfs(wire_from[wa])
-                    if wb in wire_from:
-                        dfs(wire_from[wb])
-                    if wy not in wires:
-                        wires[wy] = self.let(str(wy), Netlist.make_wire(type_, wires[wa], wires[wb]))
+                    dfs(wire_from[wa])
+                    dfs(wire_from[wb])
+                    wires[wy] = self.let(str(wy), Netlist.make_wire(type_, wires[wa], wires[wb]))
             elif type_ == "$not":
                 a, y = conns["A"], conns["Y"]
                 a, y = Netlist.bit_to_int(a[0]), Netlist.bit_to_int(y[0])
-                if a in wire_from:
-                    dfs(wire_from[a])
-                if y not in wires:
-                    wires[y] = self.let(str(y), Netlist.make_wire(type_, wires[a]))
+                dfs(wire_from[a])
+                wires[y] = self.let(str(y), Netlist.make_wire(type_, wires[a]))
             elif type_ in {"$add", "$mul"}:  # word-level arithmetic operations
                 # NOTE: it's hard to handle weird input widths, signed & unsigned, etc. in a generic way
                 # NOTE: also it's hard to deal with different styles of extension, e.g., $signed(a) vs {16{a[15]}, a}
@@ -211,31 +181,28 @@ class Netlist(egglog.EGraph):
                     adjusted_b = [Netlist.bit_to_int(bit) for bit in b] + [Netlist.bit_to_int(b[-1]) if b_signed else 0] * (len(y) - len(b))
                 else:   # apply truncation if necessary
                     adjusted_b = [Netlist.bit_to_int(bit) for bit in b[:len(y)]]
-                [dfs(wire_from[wa]) for wa in adjusted_a if wa in wire_from]
-                [dfs(wire_from[wb]) for wb in adjusted_b if wb in wire_from]
+                [dfs(wire_from[wa]) for wa in adjusted_a]
+                [dfs(wire_from[wb]) for wb in adjusted_b]
                 wva = self.let(self.auto_id, WireVec(egglog.Vec(*(wires[wa] for wa in adjusted_a))))
                 wvb = self.let(self.auto_id, WireVec(egglog.Vec(*(wires[wb] for wb in adjusted_b))))
                 wvy = Netlist.make_wirevec(type_, len(y), wva, wvb)
                 for j, wy in enumerate(y):
                     wy = Netlist.bit_to_int(wy)
-                    if wy not in wires:
-                        wires[wy] = self.let(str(wy), wvy[j])
+                    wires[wy] = self.let(str(wy), wvy[j])
             elif type_ == "$mux":
                 # NOTE: it deserves considering whether it's a bitwise mux or a word-level mux
+                # for me, I think it's better to treat it as bitwise
                 a, b, s, y = conns["A"], conns["B"], conns["S"], conns["Y"]
-                # TODO: implement this
+                assert len(s) == 1, "Mux must have exactly one select bit"
+                ws = Netlist.bit_to_int(s[0])
+                dfs(wire_from[ws])  # dfs on select wire
+                for wa, wb, wy in zip(a, b, y):
+                    wa, wb, wy = Netlist.bit_to_int(wa), Netlist.bit_to_int(wb), Netlist.bit_to_int(wy)
+                    dfs(wire_from[wa])
+                    dfs(wire_from[wb])
+                    wires[wy] = self.let(str(wy), Wire.mux(wires[wa], wires[wb], wires[ws]))
             elif type_ == "$dff":
-                if not self.param_to_int(params["CLK_POLARITY"]):
-                    raise ValueError("$dff with negative clock polarity is not supported")
-                d, clk, q = conns["D"], conns["CLK"], conns["Q"]
-                if len(clk) != 1 or Netlist.bit_to_int(clk[0]) != self._clk:
-                    raise ValueError(f"Clock {clk} does not match global clock {self._clk}")
-                for wd, wq in zip(d, q):
-                    wd, wq = Netlist.bit_to_int(wd), Netlist.bit_to_int(wq)
-                    if wd in wire_from:
-                        dfs(wire_from[wd])
-                    if wq not in wires:
-                        wires[wq] = self.let(str(wq), Wire.from_dff(wires[wd]))
+                return
             else:
                 attrs = cell["attributes"]
                 if "module_not_derived" in attrs and self.param_to_int(attrs["module_not_derived"]):    # blackbox cell
@@ -244,11 +211,17 @@ class Netlist(egglog.EGraph):
                     raise ValueError(f"Unsupported cell type: {type_}")
             visited.add(i)
 
-        for i in range(len(cells)):
-            dfs(i)
-
-        # build outputs
+        # dfs from outputs
         for name, port in ports.items():
             direction, bits = port["direction"], port["bits"]
             if direction == "output":
                 self._outputs[name] = self.let(name, WireVec(egglog.Vec(*(wires[Netlist.bit_to_int(bit)] for bit in bits))))
+
+        # dfs from dffs' d ports
+        for dff in dffs:
+            conns = dff["connections"]
+            for wd, wq in zip(conns["D"], conns["Q"]):
+                wd, wq = Netlist.bit_to_int(wd), Netlist.bit_to_int(wq)
+                dfs(wire_from[wd])
+                # union dff's q port with from_dff(d port)
+                self.register(egglog.union(wires[wq]).with_(Wire.from_dff(wires[wd])))
