adf54e5595054d21449922803b4285247deaf255
Sijie Kong <sijie_kong@ucsb.edu>
2025-08-04 12:32:37 -0700

    Refactoring rules

--- emap/rewrites/common.py
+++ emap/rewrites/common.py
@@ -5,4 +5,4 @@ from ..core import Wire, WireVec
 i, j = egglog.vars_("i j", egglog.i64)
 w0, w1 = egglog.vars_("w0 w1", Wire)
 wv0, wv1 = egglog.vars_("wv0 wv1", WireVec)
-ws0, ws1 = egglog.vars_("ws0 ws1", egglog.Vec[Wire])
+ws0, ws1, ws2, ws3 = egglog.vars_("ws0 ws1 ws2 ws3", egglog.Vec[Wire])
