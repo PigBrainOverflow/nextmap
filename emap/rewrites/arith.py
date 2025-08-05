9b9a14ddca6e473988bf239bb5140cd1ebdf1b55
Sijie Kong <sijie_kong@ucsb.edu>
2025-08-05 10:33:14 -0700

    Implemented zero/sign extension

--- emap/rewrites/arith.py
+++ emap/rewrites/arith.py
@@ -6,3 +6,66 @@ arith_comm_rules = egglog.ruleset(
     egglog.rewrite(WireVec.add(ws0, ws1)).to(WireVec.add(ws1, ws0)),
     egglog.rewrite(WireVec.mul(ws0, ws1)).to(WireVec.mul(ws1, ws0))
 )
+
+##################
+# Zero Extension #
+##################
+class zero_extended(egglog.Expr):
+    @egglog.method(unextractable=True)
+    def __init__(self, ws: egglog.Vec[Wire], width: egglog.i64Like): ...    # width is the actual width of ws (before zero extension)
+
+zero_extended_iter_rule = egglog.rule(
+    zero_extended(ws0, i), i > 0,
+    egglog.eq(ws0[i - 1]).to(Wire.from_const(0))
+).then(
+    egglog.subsume(zero_extended(ws0, i)),
+    zero_extended(ws0, i - 1)
+)
+
+zero_extended_base_rules = egglog.ruleset(
+    # NOTE: we only consider add, sub, mul here
+    egglog.rule(WireVec.add(ws0, ws1)).then(
+        zero_extended(ws0, ws0.length()),
+        zero_extended(ws1, ws1.length())
+    ),
+    egglog.rule(WireVec.sub(ws0, ws1)).then(
+        zero_extended(ws0, ws0.length()),
+        zero_extended(ws1, ws1.length())
+    ),
+    egglog.rule(WireVec.mul(ws0, ws1)).then(
+        zero_extended(ws0, ws0.length()),
+        zero_extended(ws1, ws1.length())
+    )
+)
+
+
+##################
+# Sign Extension #
+##################
+class sign_extended(egglog.Expr):
+    @egglog.method(unextractable=True)
+    def __init__(self, ws: egglog.Vec[Wire], width: egglog.i64Like): ...    # width is the actual width of ws (before sign extension)
+
+sign_extended_iter_rule = egglog.rule(
+    sign_extended(ws0, i), i > 1,
+    egglog.eq(ws0[i - 1]).to(ws0[i - 2])
+).then(
+    egglog.subsume(sign_extended(ws0, i)),
+    sign_extended(ws0, i - 1)
+)
+
+sign_extended_base_rules = egglog.ruleset(
+    # NOTE: we only consider add, sub, mul here
+    egglog.rule(WireVec.add(ws0, ws1)).then(
+        sign_extended(ws0, ws0.length()),
+        sign_extended(ws1, ws1.length())
+    ),
+    egglog.rule(WireVec.sub(ws0, ws1)).then(
+        sign_extended(ws0, ws0.length()),
+        sign_extended(ws1, ws1.length())
+    ),
+    egglog.rule(WireVec.mul(ws0, ws1)).then(
+        sign_extended(ws0, ws0.length()),
+        sign_extended(ws1, ws1.length())
+    )
+)
