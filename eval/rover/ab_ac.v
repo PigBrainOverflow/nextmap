37786f235ea4d95387595e55082110ed6d81f923
Sijie Kong <sijie_kong@ucsb.edu>
2025-10-01 04:21:26 +0000

    Clean up

--- eval/rover/ab_ac.v
+++
@@ -1,8 +0,0 @@
-module top (
-    input signed [15:0] a,
-    input signed [15:0] b,
-    input signed [15:0] c,
-    output signed [31:0] y
-);
-    assign y = a * b + a * c;
-endmodule
--- eval/rover/ab_cd.v
+++
@@ -1,9 +0,0 @@
-module top (
-    input signed [15:0] a,
-    input signed [15:0] b,
-    input signed [15:0] c,
-    input signed [15:0] d,
-    output signed [31:0] y
-);
-    assign y = a * b + c * d;
-endmodule
